d6395ee9a65d89bdad61a737930c201eb6e09789
sergioflogu19-commits <sasayin19@hotmail.com>
2022-03-31 23:34:24 -0400

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <seo-teleport/>
+    <navigation />
+    <router-view/>
+</template>
+
+<script>
+    import Navigation from "@/components/Navigation";
+    import SeoTeleport from "./components/SeoTeleport";
+
+    export default {
+        components: {SeoTeleport, Navigation}
+    }
+</script>
